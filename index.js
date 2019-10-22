48012023bd9134a422f399563110190838e828ec
Monica Marziano <mmarziano1@gmail.com>
2019-10-22 14:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(arr, name) {
+  return arr.filter(n => n.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(arr, input) {
+  
+  
+}
+
+function matchName() {
+  
+  
+}
