77041e0a997354b49750f754d81ee2149112221b
Monica Marziano <mmarziano1@gmail.com>
2019-10-22 15:55:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(arr, name) {
+  return arr.filter(n => n.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(arr, input='') {
+  return arr.filter(word => word.slice(0, input.length) === input)
+}
+
+function matchName(arr, input) {
+  return arr.filter(obj => obj.name.toLowerCase() === input.toLowerCase())
+  
+}
