73fa443185b493c3e2325677392b1af0812391a2
Monica Marziano <mmarziano1@gmail.com>
2019-10-22 15:37:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function findMatching(arr, name) {
   return arr.filter(n => n.toLowerCase() === name.toLowerCase())
 }
 
-function fuzzyMatch(arr, input) {
+function fuzzyMatch(arr, input='') {
   let w = arr.map(word => word.slice(0, input.length))
   let f = w.filter(l => l === input)
   return f
